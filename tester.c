9f945eab5ba219f26223c9448bb7a67d544a483f
Marc Schoolderman <squell@xs4all.nl>
2016-01-20 21:53:01 +0100

    split output between stdout and stderr

--- tester.c
+++ tester.c
@@ -11,11 +11,12 @@ extern volatile unsigned long avr_INTR;
 
 extern unsigned long avr_PC;
 extern unsigned char avr_ADDR[];
-extern unsigned char avr_FLASH[];
+extern unsigned short int avr_FLASH[];
 extern unsigned short int avr_SP;
 extern unsigned char avr_SREG;
 
-/* watchdog as a seperate thread */
+/* a watchdog process; behaves mostly according to the datasheet,
+   except that the timed sequence involving WDCE isn't implemented */
 
 pthread_t thread;
 
@@ -25,97 +26,116 @@ pthread_t thread;
 unsigned char wdtcr;
 
 enum wdtcr_bits {
-    WDIF = 1<<7, WDIE = 1<<6, WDCE = 1<<4, WDE  = 1<<3
+	WDIF = 1<<7, WDIE = 1<<6, WDCE = 1<<4, WDE	= 1<<3
 };
 
 void *watchdog(void *threadid)
 {
-    unsigned long last_wdr = 0;
-    unsigned long timer = 0;
-    for(;;) {
-        usleep(1024);
-        wdtcr = WDTCR;
-        unsigned long cur = avr_last_wdr;
-        unsigned long threshold = 2ul << ((wdtcr&0x20)/4 + (wdtcr&0x7)) % 10;
-        if(cur == last_wdr && wdtcr&0x48 && ++timer > threshold) {
-            timer = 0;
-            if(wdtcr & WDIE) {
-                wdtcr |= WDIF;
-                if(wdtcr & WDE)
-                    wdtcr &=~WDIE;
-                WDTCR = wdtcr;
-                timer = 0;
-                avr_INT = 1;
-            } else if(wdtcr & WDE) {
-                wdtcr = 0;
-                timer = 0;
-                avr_INT = 1;
-                while(avr_INT)  /* force-quit the emulator */
-                    avr_IO[0x3F] = 0x80;
-            }
-        } else if(cur != last_wdr) {
-            timer = 0;
-            last_wdr = cur;
-        }
-    }
-    return 0;
+	unsigned long last_wdr = 0;
+	unsigned long timer = 0;
+	fprintf(stderr, "%s\n", "starting watchdog");
+	for(;;) {
+		usleep(1024);
+		wdtcr = WDTCR;
+		unsigned long cur = avr_last_wdr;
+		unsigned long threshold = 2ul << ((wdtcr&0x20)/4 + (wdtcr&0x7)) % 10;
+		if(cur == last_wdr && wdtcr&0x48 && ++timer > threshold) {
+			timer = 0;
+			if(wdtcr & WDIE) {
+				wdtcr |= WDIF;
+				if(wdtcr & WDE)
+					wdtcr &=~WDIE;
+				WDTCR = wdtcr;
+				timer = 0;
+				avr_INT = 1;
+			} else if(wdtcr & WDE) {
+				wdtcr = 0;
+				timer = 0;
+				avr_INT = 1;
+				while(avr_INT)	/* force-quit the emulator */
+					avr_IO[0x3F] = 0x80;
+			}
+		} else if(cur != last_wdr) {
+			timer = 0;
+			last_wdr = cur;
+		}
+	}
+	return 0;
 }
 
-/* simple avr emulation */
+/* dump the emulation state */
 
 void avr_debug(unsigned long ip)
 {
-    int i;
-    printf("%10lld: ", avr_cycle);
-    for(i=0; i < 32; i++) printf("%02x ", avr_ADDR[i+0x00]);
-    printf("SP=%04x", avr_SP);
-    //printf("FLAG=%02x", avr_SREG);
-    //printf("SP=%04x, IP=%04lx", avr_SP, ip);
-    //for(i=0; i < 3; i++) printf("%02x ", avr_ADDR[avr_SP+i]);
-    printf("\n");
+	int i;
+	fprintf(stderr, "%10lld: ", avr_cycle);
+	for(i=0; i < 32; i++)
+	fprintf(stderr, "%02x ", avr_ADDR[i+0x00]);
+	fprintf(stderr, "SP=%04x, SREG=%02x, PC=%04lx [%04x]", avr_SP, avr_SREG, ip, avr_FLASH[ip]);
+	fprintf(stderr, "\n");
+}
+
+ /* two io functions to fake a proper USART */
+
+void avr_io_in(int port)
+{
+	if(port == 0xA0)
+		avr_IO[port] = -1;
+}
+
+void avr_io_out(int port)
+{
+	switch(port) {
+	case 0xA6:
+		putchar(avr_IO[port]);
+		fflush(stdout);
+	}
 }
 
 int main(int argc, char **argv)
 {
-    int i;
-    int n = ihex_read(argv[1], avr_FLASH, 0x40000);
-    if(n < 0)  {
-        printf("could not read %s\n", argv[1]);
-        return 2;
-    }
-    printf("%d bytes read\n", n);
-    avr_reset();
-    puts("starting watchdog");
-    MCUSR |= 0x1;  /* set PORF */
-    WDTCR |= WDE;  /* enable the watchdog */
-    pthread_create(&thread, 0, watchdog, 0);
-    do {
-        switch( avr_run() ) {
-        case 0:
-            if(wdtcr) {
-                puts("watchdog interrupt");
-                avr_PC = 0xC;
-                WDTCR &=~WDIF;
-            } else {
-                puts("watchdog reset");
-                avr_reset();
-                MCUSR |= 0x8; /* set WDRF */
-                break;
-            }
-            continue;
-        case 1:
-            puts("mcu idle");
-            break;
-        case 2:
-            puts("breakpoint");
-            break;
-        default:
-            printf("unexpected situation: PC=%04lx instruction=%04x\n", avr_PC, avr_FLASH[avr_PC]);
-            break;
-        }
-        break;
-    } while(1);
-    puts("done");
-    avr_debug(avr_PC);
-    return 0;
+    	if(!argv[1]) {
+		fprintf(stderr, "usage: tester [file.hex]\n");
+	    	return 2;
+	}
+	int n = ihex_read(argv[1], avr_FLASH, 0x10000);
+	if(n < 0)  {
+		fprintf(stderr, "could not read %s\n", argv[1]);
+		return 2;
+	}
+	fprintf(stderr, "%d bytes read\n", n);
+
+	avr_reset();
+	MCUSR |= 0x1;  /* set PORF */
+	/* WDTCR |= WDE; uncomment this to start the watchdog timer by default */
+	pthread_create(&thread, 0, watchdog, 0);
+	do {
+		switch( avr_run() ) {
+		case 0:
+			if(wdtcr) {
+				fprintf(stderr, "%s\n", "watchdog interrupt");
+				avr_PC = 0xC;
+				WDTCR &=~WDIF;
+			} else {
+				fprintf(stderr, "%s\n", "watchdog reset");
+				avr_reset();
+				MCUSR |= 0x8; /* set WDRF */
+				break;
+			}
+			continue;
+		case 1:
+			fprintf(stderr, "%s\n", "mcu idle");
+			break;
+		case 2:
+			fprintf(stderr, "%s\n", "breakpoint");
+			break;
+		default:
+			fprintf(stderr, "unexpected situation: PC=%04lx instruction=%04x\n", avr_PC, avr_FLASH[avr_PC]);
+			break;
+		}
+		break;
+	} while(1);
+	fprintf(stderr, "%s\n", "done");
+	avr_debug(avr_PC-1);
+	return 0;
 }
